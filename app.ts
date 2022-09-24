7b3b0639c45aa6a6fae3a632dc61535b01d5d5e5
TakshakRamteke <takshakramteke0708@gmail.com>
2022-09-25 01:34:55 +0530

    FEAT : Backend for app

--- app.ts
+++ app.ts
@@ -1,9 +1,28 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import bodyparser from "body-parser";
 
 const app: Express = express();
+const prisma = new PrismaClient();
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("I love unit07");
+  res.render("index");
+});
+
+app.get("/:id", async (req: Request, res: Response) => {
+  const temp = await prisma.links.findUnique({ where: { id: req.params.id } });
+  const link = temp?.link;
+  res.redirect(`${link}`);
+});
+
+app.post("/api/shorten", async (req: Request, res: Response) => {
+  const newShort = await prisma.links.create({ data: { link: req.body.link } });
+  const link = newShort.id;
+  res.render("shortened", { link });
 });
 
 app.listen(5000, () => {
