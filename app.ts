efd99bb5f26f999c590f2383f87a4d21741ef656
TakshakRamteke <takshakramteke0708@gmail.com>
2022-10-07 15:18:53 +0530

    FIX : Minor fixes for deployment

--- app.ts
+++ app.ts
@@ -34,8 +34,6 @@ app.listen(5000, () => {
   console.log("Server running at http://localhost:5000");
 });
 
-module.exports = app;
-
 // Generate a random string of given length
 function randString(length: number) {
   const characterSet =
